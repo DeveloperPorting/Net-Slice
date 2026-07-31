1458786efd025d34d5a6a0ebed52548c7f6e42c7
StarNovaBR <cherrylegal001@gmail.com>
2026-07-31 08:30:15 -0300

    vai se fuder

--- source/lime/media/vorbis/VorbisFile.hx
+++ source/lime/media/vorbis/VorbisFile.hx
@@ -344,4 +344,17 @@ class VorbisComment
 
 	public function new() {}
 }
+
+class VorbisInfo
+{
+	public var bitrateLower:Int;
+	public var bitrateNominal:Int;
+	public var bitrateUpper:Int;
+	// public var bitrateWindow:Int;
+	public var channels:Int;
+	public var rate:Int;
+	public var version:Int;
+
+	public function new() {}
+}
 #end
